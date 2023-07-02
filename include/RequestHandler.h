449eb3221e0ddce0644a5f3bc03c9acc3c0f01cf
HeiSeYuMiLi <baoguli@outlook.com>
2023-07-02 21:27:50 +0800

    xinzeng

---
+++ include/RequestHandler.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <vector>
+#include <string>
+#include "Request.h"
+#include "Reply.h"
+
+namespace file_transfer_server {
+
+	class RequestHandler
+	{
+	public:
+		//bool HandleRequest(const std::string&, std::vector<char>&);
+		bool HandleRequest(Request& req,Reply& rep);
+
+	private:
+		enum {
+			head_length = 4
+		};
+	};
+
+}
+
+
+
